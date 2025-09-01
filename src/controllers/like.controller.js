8a3ab344e79849d76cb3963713182109f2c5efe5
Rachit0919 <rachit0919@gmail.com>
2025-09-01 21:50:41 +0530

    feat: implement toggle like functionality for videos; add error handling for invalid video ID and user authentication

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -1,17 +1,81 @@
 import mongoose, {isValidObjectId} from "mongoose"
 import {Like} from "../models/like.model.js"
+import {Video} from "../models/video.model.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
-    const {videoId} = req.params
-    //TODO: toggle like on video
+    const {videoId} = req.params   // from url
+    if (!videoId ) {
+        throw new ApiError(400, "Invalid video ID")
+    }
+
+    const {userId} = req.user  // from middleware
+    if (!userId) {
+        throw new ApiError(401, "Please log in to like videos")
+    }
+
+    const updatedVideoLike = await Like.findOneAndUpdate(
+        {
+            video: videoId,
+            likedBy: userId
+        },
+        [
+            {
+                $set:{
+                    video: videoId,
+                    likedBy: userId,
+                    isLiked: {
+                        $not: ["$isLiked"]
+                    },
+                    toggledAt: new Date()
+                }
+            }
+        ],
+        {
+            upsert:true,
+            new: true
+        }
+
+    )
+    const message = updatedVideoLike.isLiked? "User liked the video": "User unliked the video"
+
+    
+    //  this approach is also correct but call db two time just for one action
+
+    // const hasUserLikedTheVideo = await Like.findOne({
+    //     video: videoId,
+    //     likedBy: userId
+    // })
+
+    // if (!hasUserLikedTheVideo) {
+    //     await Like.create({
+    //         video: videoId,
+    //         likedBy: userId
+    //     }
+    // )
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200, 
+            message,
+            updatedVideoLike
+        )
+    )
 })
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
     const {commentId} = req.params
-    //TODO: toggle like on comment
+    if (!commentId) {
+        throw new ApiError(401, "Invalid comment Id")
+    }
+    const {userId} = req.user
+    if (!userId) {
+        throw new ApiError()
+    }
+
 
 })
 
